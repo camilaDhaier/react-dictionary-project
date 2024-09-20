8de2d55d29d6dad350563b84d187e7c587843af6
camilaDhaier <violincamila@gmail.com>
2024-09-20 15:15:22 -0300

    created the form with the functions and events defined

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './shecodes-logo.gif';
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
+    <div className="container">
     <div className="App">
       <header className="App-header">
         <img src={logo}
         className="App-logo img-fluid" alt="logo" />
-        
-      </header>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+
+
+      <footer className="App-footer">
+        <small >Coded by Camila Dhaier </small>
+        </footer>
+    </div>
     </div>
   );
 }
